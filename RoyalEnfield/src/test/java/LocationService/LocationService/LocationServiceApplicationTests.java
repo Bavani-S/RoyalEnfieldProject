04719718e8106043c11faa65b100444aef081731
Bavani.S <mail2bavani.s@gmail.com>
2021-07-16 14:15:48 +0530

    Committes test class

--- RoyalEnfield/src/test/java/LocationService/LocationService/LocationServiceApplicationTests.java
+++ RoyalEnfield/src/test/java/LocationService/LocationService/LocationServiceApplicationTests.java
@@ -3,7 +3,7 @@ package LocationService.LocationService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes=RoyalEnfield.RoyalEnfield.RoyalEnfield.class)
 class LocationServiceApplicationTests {
 
 	@Test
